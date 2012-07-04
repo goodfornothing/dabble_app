3af2663a890780de196e9e784c3b93ed2f7f0441
Stef Lewandowski <stef@stef.io>
2012-07-04 17:09:19 +0100

    pictures

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,20 +1,31 @@
-App = Ember.Application.create();
-
-App.selectedMealController = Ember.Object.create({
-    meal: null,
-    hint: function() {
-      var meal = this.get('meal');
+App = Ember.Application.create({
+  another: function() {
+      var meal = App.selectedMealController.get('meal');
       if(meal) {
-        return(meal.get("summer")[Math.floor(Math.random()*meal.get("summer").length)]);
+        App.selectedHintController.set("hint", meal.get("summer")[Math.floor(Math.random()*meal.get("summer").length)]);
       }
-    }.property('meal'),
-    another: function() {
-
     }
 });
 
+App.selectedMealController = Ember.Object.create({
+    meal: null,
+    mealChanged: function() {
+      var meal = this.get('meal');
+      if(meal) {
+        App.selectedHintController.set("hint", meal.get("summer")[0]);
+      }
+    }.observes('meal')
+});
+
 App.selectedHintController = Ember.Object.create({
-  hint: null
+  hint: null,
+  vegetable: function() {
+    console.log("Vegetable");
+    var hint = this.get('hint');
+    if(hint) {
+      return '/veg/' + hint.vegetable + ".png";
+    }
+  }.property('hint')
 });
 
 App.Meal = Ember.Object.extend({
@@ -24,14 +35,30 @@ App.Meal = Ember.Object.extend({
     summer: null
 });
 
+App.ImgView = Em.View.extend({
+    tagName: 'img',
+    didInsertElement: function() {
+        console.log(this.get('src'));
+    }
+});
+
 App.mealsController = Ember.ArrayController.create({
     content: [
-        App.Meal.create({id: 1, name: 'Spaghetti Bolognese', slug: 'spag-bol', summer: ['Grate a courgette into your mince', 'Add a carrot to the mix']}),
-        App.Meal.create({id: 2, name: 'Roast dinner', slug: 'roast', summer: ['Roast some cherry tomatoes on the vine']}),
-        App.Meal.create({id: 3, name: 'Casserole', slug: 'casserole', summer: ['Add a big handful of fresh spinach at the end']}),
-        App.Meal.create({id: 4, name: 'Curry', slug: 'curry', summer: ['Add some quartered tomatoes for the last 10 minutes']}),
-        App.Meal.create({id: 5, name: 'Meat and two veg', slug: 'meat', summer: ['Pod some fresh peas and broad beans and boil for 3 minutes']}),
-        App.Meal.create({id: 6, name: 'Bangers and mash', slug: 'bangers', summer: ['Chop some watercress leaves and add them to your mash']})
+        App.Meal.create({id: 1, name: 'Spaghetti Bolognese', slug: 'spag-bol', summer: [
+          {idea: 'Grate a courgette into your mince', vegetable: 'courgette'},
+          {idea: 'Add a carrot to the mix', vegetable: 'carrot'},
+          {idea: 'Chop up a pepper and add it to the sauce', vegetable: 'pepper'}]}),
+        App.Meal.create({id: 2, name: 'Roast dinner', slug: 'roast', summer: [
+          {idea: 'Roast some cherry tomatoes on the vine', vegetable: 'cherry-tomatoes'},
+          {idea: 'Put an onion in with the potatoes', vegetable: 'potato'}]}),
+        App.Meal.create({id: 3, name: 'Casserole', slug: 'casserole', summer: [
+          {idea: 'Add a big handful of fresh spinach at the end', vegetable: 'spinach'}]}),
+        App.Meal.create({id: 4, name: 'Curry', slug: 'curry', summer: [
+          {idea: 'Add some quartered tomatoes for the last 10 minutes', vegetable: 'quartered-tomatoes'}]}),
+        App.Meal.create({id: 5, name: 'Meat and two veg', slug: 'meat', summer: [
+          {idea: 'Pod some fresh peas and broad beans and boil for 3 minutes', vegetable: 'peas'}]}),
+        App.Meal.create({id: 6, name: 'Bangers and mash', slug: 'bangers', summer: [
+          {idea: 'Chop some watercress leaves and add them to your mash', vegetable: 'watercress'}]})
     ]
 });
 
